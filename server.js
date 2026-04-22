73966ce1edc96fae3d9354ccfe7b973625c05268
Theekshana-Indrajith <agtheekshanaindrajith@gmail.com>
2026-04-22 12:35:33 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number
+});
+const Item = mongoose.model('Item', itemSchema);
+
+app.get('/items', async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+app.post('/items', async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.json(newItem);
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
